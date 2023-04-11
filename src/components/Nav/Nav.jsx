0a4b3486852cd5d2ee0da0ec8b3fc5269a613f7f
Nagima Zhumakadyrova <zhumakadyrova7@gmail.com>
2023-04-11 14:53:58 +0600

    begin to work

---
+++ src/components/Nav/Nav.jsx
@@ -0,0 +1,13 @@
+import { NavLink } from "react-router-dom";
+import "./Nav.css";
+
+export default function Nav() {
+  return (
+    <nav className="Nav">
+      <ul>
+        <li><NavLink to="/">Home</NavLink></li>
+        <li><NavLink to="/about">About</NavLink></li>
+      </ul>
+    </nav>
+  );
+}
---
+++ src/pages/Home.jsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className="Home">
+      <h1>Welcome to my Ice-Cream shop</h1>
+      <p>This is my awesome Ice-Cream shop.</p>
+      <p>We only use natural ingredients to make our yummy ice-creams</p>
+    </div>
+  );
+}
