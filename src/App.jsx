5751da1d1dad3407ac751316f9c23f43c801c198
Nagima Zhumakadyrova <zhumakadyrova7@gmail.com>
2023-04-11 14:28:34 +0600

    filled App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
-import './App.css'
+import { Route, Routes } from "react-router-dom";
+import Layout from "./components/Layout/Layout";
+import Home from "./pages/Home";
 
-function App() {
-
+export default function App() {
   return (
     <div className="App">
-      
+      <Layout>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<h2>About</h2>} />
+        </Routes>
+      </Layout>
     </div>
-  )
+  );
 }
-
-export default App;
