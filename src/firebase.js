a9c58482ecb01ae9f1fa12da30a50a9581675f34
Nagima Zhumakadyrova <zhumakadyrova7@gmail.com>
2023-04-11 15:35:57 +0600

    add category list a const

--- src/firebase.js
+++ src/firebase.js
@@ -19,5 +19,5 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
-
+//получить списк категорий (коллекция документов).
 export const categoryCollection = collection(db, "categories");
