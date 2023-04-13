0cd6aa6fa3fb377dae3dc916bb9087f1c49d9b2b
Nagima Zhumakadyrova <zhumakadyrova7@gmail.com>
2023-04-13 15:19:15 +0600

    check easy methods to find the way for categories

--- src/pages/Category.jsx
+++ src/pages/Category.jsx
@@ -3,11 +3,19 @@ import { useMatch } from "react-router-dom";
 import { AppContext } from "../App";
 
 export default function Category() {
-  const { params } = useMatch("/category/:path");
-  const { categories } = useContext(AppContext)
+  //деструктизация
+  const { params } = useMatch("/category/:path"); 
+  const { categories } = useContext(AppContext);
+
+  // //простой метод
+  // const match = useMatch ('/category/:path');
+
+  const category = categories.find(
+    category => params.path === category.path
+  )
   return (
     <div className="Category">
-      <h1>Category</h1>
+      <h1>{category ? category.name: "Loading..."}</h1>
     </div>
   );
 }
