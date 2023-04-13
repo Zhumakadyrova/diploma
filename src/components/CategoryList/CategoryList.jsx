6cd1aecec112e99fe726dc49f885f1f45c3775a9
Nagima Zhumakadyrova <zhumakadyrova7@gmail.com>
2023-04-13 14:45:21 +0600

    changed in categoryList

--- src/components/CategoryList/CategoryList.jsx
+++ src/components/CategoryList/CategoryList.jsx
@@ -1,31 +1,12 @@
 import { getDocs } from "firebase/firestore";
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { NavLink } from "react-router-dom";
+import { AppContext } from "../../App";
 import { categoryCollection } from "../../firebase";
 import "./CategoryList.css";
 export default function CategoryList() {
-  const [categories, setCategories] = useState([]);
+  const {categories} = useContext(AppContext);
 
-  //выполнить эту функцию только один раз
-  useEffect(() => {
-    //получить категории из списка категорий
-    getDocs(categoryCollection).then((snapshot) => {
-      //категории будут храниться в snapshot.докс
-
-      //создать массив для категорий
-      const newCategories = [];
-
-      snapshot.docs.forEach((doc) => {
-        // doc = категорий
-        const category = doc.data();
-        category.id = doc.id;
-
-        newCategories.push(category);
-      });
-      //задать новый массив как состояние компонент
-      setCategories(newCategories);
-    });
-  }, []);
   const output = categories.map((category) => (
     <li key={category.id}>
       <NavLink to={"/category/" + category.path}>{category.name}</NavLink>
