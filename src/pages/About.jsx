8f8315c5933d9cc8ce0120a59d93e9cc6e045995
Nagima Zhumakadyrova <zhumakadyrova7@gmail.com>
2023-04-13 22:45:08 +0600

    to check the nav specialties in react

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,7 +1,7 @@
 export default function About() {
   return (
     <div className="About">
-      <h1>Welcome to my Ice-Cream shop</h1>
+      <h1>Welcome to my Iam shop</h1>
       <p>This is my awesome Ice-Cream shop.</p>
       <p>We only use natural ingredients to make our yummy ice-creams</p>
     </div>
