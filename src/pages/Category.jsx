ba0e94b0b9cc8f05ea9130e75b385e9f64316763
Nagima Zhumakadyrova <zhumakadyrova7@gmail.com>
2023-04-13 16:00:35 +0600

    add products collection

--- src/pages/Category.jsx
+++ src/pages/Category.jsx
@@ -1,6 +1,7 @@
 import { useContext } from "react";
 import { useMatch } from "react-router-dom";
 import { AppContext } from "../App";
+import ProductList from "../components/ProductList/ProductList";
 
 export default function Category() {
   //деструктизация
@@ -16,6 +17,7 @@ export default function Category() {
   return (
     <div className="Category">
       <h1>{category ? category.name: "Loading..."}</h1>
+      <ProductList/>
     </div>
   );
 }
