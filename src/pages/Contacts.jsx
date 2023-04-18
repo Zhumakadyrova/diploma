cd6525046b597e3e8d07b8159679e8daf1791bf0
Nagima Zhumakadyrova <zhumakadyrova7@gmail.com>
2023-04-18 20:44:53 +0600

    add contacts page to the nav

---
+++ src/pages/Contacts.jsx
@@ -0,0 +1,9 @@
+export default function Contacts() {
+  return (
+    <div className="Contacts">
+      <h1>Welcome to my Contacts shop</h1>
+      <p>This is my awesome Ice-Cream shop.</p>
+      <p>We only use natural ingredients to make our yummy ice-creams</p>
+    </div>
+  );
+}
