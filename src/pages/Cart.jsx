11d5c4c919dbe69bf55c585a64fc39071b25f429
Nagima Zhumakadyrova <zhumakadyrova7@gmail.com>
2023-04-21 11:41:37 +0600

    add roder form to the cart to use

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,4 +1,5 @@
 import CartList from "../components/CartList/CartList";
+import OrderForm from "../components/OrderForm/OrderForm";
 import "./Cart.css";
 
 export default function Cart() {
@@ -6,6 +7,7 @@ export default function Cart() {
     <div className="Cart">
       <h1>Cart</h1>
       <CartList/>
+      <OrderForm/>
     </div>
   )
 }
