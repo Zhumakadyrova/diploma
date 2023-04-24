bc0de9be2e3af25097aa58c9755dc2133c76a086
Nagima Zhumakadyrova <zhumakadyrova7@gmail.com>
2023-04-24 17:58:06 +0600

    format the document

--- src/pages/Category.jsx
+++ src/pages/Category.jsx
@@ -6,23 +6,21 @@ import NotFound from "./NotFound";
 
 export default function Category() {
   //деструктизация
-  const { params } = useMatch("/category/:path"); 
+  const { params } = useMatch("/category/:path");
   const { categories } = useContext(AppContext);
 
   // //простой метод
   // const match = useMatch ('/category/:path');
 
-  const category = categories.find(
-    category => params.path === category.path
-  )
+  const category = categories.find((category) => params.path === category.path);
 
   if (!category) {
-    return <NotFound/>
+    return <NotFound />;
   }
   return (
     <div className="Category">
-      <h1>{category ? category.name: "Loading..."}</h1>
-      <ProductList category={category}/>
+      <h1>{category ? category.name : "Loading..."}</h1>
+      <ProductList category={category} />
     </div>
   );
 }
