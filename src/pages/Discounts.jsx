e5478028eec755e021daec60bce1f00e4bb0f3c0
Nagima Zhumakadyrova <zhumakadyrova7@gmail.com>
2023-04-24 17:58:54 +0600

    created new page

---
+++ src/pages/Discounts.jsx
@@ -0,0 +1,9 @@
+export default function About() {
+  return (
+    <div className="Discounts">
+      <h1>Welcome to my I am shop</h1>
+      <p>This is my awesome Ice-Cream shop.</p>
+      <p>We only use natural ingredients to make our yummy ice-creams</p>
+    </div>
+  );
+}
