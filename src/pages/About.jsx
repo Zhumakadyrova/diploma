5a4a371d597c3300a8ccda76d5b422ab38705165
Nagima Zhumakadyrova <zhumakadyrova7@gmail.com>
2023-04-24 17:55:43 +0600

    delete about jsx

--- src/pages/About.jsx
+++
@@ -1,9 +0,0 @@
-export default function About() {
-  return (
-    <div className="About">
-      <h1>Welcome to my I am shop</h1>
-      <p>This is my awesome Ice-Cream shop.</p>
-      <p>We only use natural ingredients to make our yummy ice-creams</p>
-    </div>
-  );
-}
