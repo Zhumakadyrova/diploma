d6dc0e5d70d94e36e6ed8b89f939d65c805769c4
Nagima Zhumakadyrova <zhumakadyrova7@gmail.com>
2023-04-26 19:24:40 +0600

    add video to the ho,e as a background

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,18 @@
+import "./Home.css"
 export default function Home() {
   return (
     <div className="Home">
+      <div className="container">
+      <iframe width="560" height="415" src="https://www.youtube.com/embed/RSCoug4NgRE" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+      </div>
+      <div className="content">
       <h1>Welcome to my Ice-Cream shop</h1>
       <p>This is my awesome Ice-Cream shop.</p>
       <p>We only use natural ingredients to make our yummy ice-creams</p>
+      </div>
     </div>
+    
+    
   );
 }
+
