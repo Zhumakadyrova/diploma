9f103596696a7ad403863fa59838e24b6997654b
Nagima Zhumakadyrova <zhumakadyrova7@gmail.com>
2023-04-28 14:21:53 +0600

    remove iframe video for th ehome'jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,9 +2,9 @@ import "./Home.css"
 export default function Home() {
   return (
     <div className="Home">
-      <div className="container">
+      {/* <div className="container">
       <iframe width="560" height="415" src="https://www.youtube.com/embed/RSCoug4NgRE" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
-      </div>
+      </div> */}
       <div className="content">
       <h1>Welcome to my Ice-Cream shop</h1>
       <p>This is my awesome Ice-Cream shop.</p>
