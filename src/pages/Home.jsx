58be0ea38bbcd5bb678f0d1217982c4aba672c10
Nagima Zhumakadyrova <zhumakadyrova7@gmail.com>
2023-05-02 20:01:14 +0600

    remove video teg

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,8 @@
 import "./Home.css";
-// import  clip from "./travel-background.mp4";
 export default function Home() {
   return (
     <div className="Home">
       <div className="content">
-        {/* <video autoPlay="autoplay" loop muted>
-        <source src={clip} type='travel' />
-        </video> */}
         <h1>Welcome to my Ice-Cream shop</h1>
         <p>This is my awesome Ice-Cream shop.</p>
         <p>We only use natural ingredients to make our yummy ice-creams</p>
