d5d9a13b32cfebb4b9fd433bd131d689aa499f37
Nagima Zhumakadyrova <zhumakadyrova7@gmail.com>
2023-05-02 19:59:26 +0600

    work on style of category list and add adding and removing function

--- src/components/CategoryList/CategoryList.jsx
+++ src/components/CategoryList/CategoryList.jsx
@@ -1,7 +1,8 @@
 import { useContext } from "react";
 import { NavLink } from "react-router-dom";
 import { AppContext } from "../../App";
-import "./CategoryList.css";
+import DeleteCategory from "../DeleteCategory/DeleteCategory";
+import AddCategory from "../AddCategory/AddCategory";
 
 export default function CategoryList() {
   const { categories } = useContext(AppContext);
@@ -9,9 +10,16 @@ export default function CategoryList() {
   const output = categories.map((category) => (
     <li key={category.id}>
       <NavLink to={"/category/" + category.path}>{category.name}</NavLink>
+      <DeleteCategory category={category}/>
+      <ul className="subcategory">
+        <li>123</li>
+        <li>34545</li>
+        <li>3454</li>
+      </ul>
     </li>
   ));
   return (<div className="CategoryList">
       <ul id="dropdown" className="dropdownContent">{output}</ul>
+      <AddCategory/>
     </div>)
 }
