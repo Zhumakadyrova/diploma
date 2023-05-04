ba28a8d5fd5fc47c9966115ab7b82a662a15aaa5
Nagima Zhumakadyrova <zhumakadyrova7@gmail.com>
2023-05-05 01:49:44 +0600

    add new indepentent function to show the product.name onMouseOver

--- src/components/CategoryList/CategoryList.jsx
+++ src/components/CategoryList/CategoryList.jsx
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import "./CategoryList.css";
 import { NavLink } from "react-router-dom";
 import { AppContext } from "../../App";
@@ -6,21 +6,48 @@ import DeleteCategory from "../DeleteCategory/DeleteCategory";
 import AddCategory from "../AddCategory/AddCategory";
 
 export default function CategoryList() {
-  const { categories } = useContext(AppContext);
+  const { categories, products } = useContext(AppContext);
+  const [showSubcategories, setShowSubcategories] = useState({});
 
-  const output = categories.map((category) => (
-    <li key={category.id}>
-      <NavLink to={"/category/" + category.path}>{category.name}</NavLink>
-      <DeleteCategory category={category}/>
-      <ul className="subcategory">
-        <li>123</li>
-        <li>34545</li>
-        <li>3454</li>
+  const handleMouseEnter = (categoryId) => {
+    setShowSubcategories((prev) => ({ ...prev, [categoryId]: true }));
+  };
+
+  const handleMouseLeave = (categoryId) => {
+    setShowSubcategories((prev) => ({ ...prev, [categoryId]: false }));
+  };
+
+  const output = categories.map((category) => {
+    const relatedProducts = products.filter((product) => product.category === category.id);
+    const relatedProductList = relatedProducts.map((product) => (
+      <li key={product.id}>
+        <NavLink to={"/product/" + product.id}>{product.name}</NavLink>
+      </li>
+    ));
+
+    const showSubcategory = showSubcategories[category.id];
+
+    return (
+      <li
+        key={category.id}
+        onMouseEnter={() => handleMouseEnter(category.id)}
+        onMouseLeave={() => handleMouseLeave(category.id)}
+      >
+        <NavLink to={"/category/" + category.path}>{category.name}</NavLink>
+        <DeleteCategory category={category} />
+        <ul className="subcategory" style={{ display: showSubcategory ? "block" : "none" }}>
+          {relatedProductList}
+        </ul>
+      </li>
+    );
+  });
+
+  return (
+    <div className="CategoryList">
+      <ul id="dropdown" className="dropdownContent">
+        {output}
       </ul>
-    </li>
-  ));
-  return (<div className="CategoryList">
-      <ul id="dropdown" className="dropdownContent">{output}</ul>
-      <AddCategory/>
-    </div>)
+      <AddCategory />
+    </div>
+  );
 }
