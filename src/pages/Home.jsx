aa2d0ea43a52c30cc5ae385a18bd572d8dcdc802
Nagima Zhumakadyrova <zhumakadyrova7@gmail.com>
2023-05-05 01:59:20 +0600

    work on content of the page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div className="Home">
       <div className="content">
-        <h1>Welcome to my Ice-Cream shop</h1>
+        <h1>Welcome to Try Travel</h1>
         <p>This is my awesome Ice-Cream shop.</p>
         <p>We only use natural ingredients to make our yummy ice-creams</p>
       </div>
