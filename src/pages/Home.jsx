41b2073a4af35a95a24c161506a13fb6c59b122d
Nagima Zhumakadyrova <zhumakadyrova7@gmail.com>
2023-05-10 12:57:22 +0600

    add addintional link teg

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,6 +8,9 @@ export default function Home() {
         <p>This is my awesome Ice-Cream shop.</p>
         <p>We only use natural ingredients to make our yummy ice-creams</p>
       </div>
+      <div className="Link">
+        <a href="./Discount.jsx">next</a>
+      </div>
     </div>
   );
 }
