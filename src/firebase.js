3ba85ef0b3423e34d3acc781faf297f316f714c4
Nagima Zhumakadyrova <zhumakadyrova7@gmail.com>
2023-05-11 20:51:51 +0600

    add import to this part

--- src/firebase.js
+++ src/firebase.js
@@ -2,6 +2,7 @@
 import { initializeApp } from "firebase/app";
 import { collection, getFirestore, onSnapshot } from "firebase/firestore";
 import { GoogleAuthProvider, getAuth, onAuthStateChanged, signInWithPopup, signOut } from "firebase/auth";
+import { getStorage, ref, uploadBytes, getDownloadURL } from "firebase/storage";
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -23,6 +24,7 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
 const auth = getAuth(app);
+export const storage = getStorage(app);
 
 // получить список категорий (коллекция документов).
 export const categoryCollection = collection(db, 'categories');
@@ -61,3 +63,12 @@ export const onOrdersLoad = (callback) =>
       }))
     )
   );
+
+  // отправка фотографии и получение ее url
+export const uploadProductPhoto = async (file) => {
+  const storageRef = ref(storage, `products/${file.name}`);
+  await uploadBytes(storageRef, file);
+
+  const url = await getDownloadURL(storageRef);
+  return url;
+};
