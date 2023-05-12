cdb55aad5d15aa7ddc7dcf93dca8ddbc3d944181
Nagima Zhumakadyrova <zhumakadyrova7@gmail.com>
2023-05-12 14:12:34 +0600

    rechange the link of the items

--- src/pages/Contacts.jsx
+++ src/pages/Contacts.jsx
@@ -8,9 +8,10 @@ export default function Contacts() {
         <p>We only use natural ingredients to make our yummy ice-creams</p>
       </div>
       <div className="Main_details">
-          <li><a href="#"><img src="https://i.pinimg.com/564x/29/47/9b/29479ba0435741580ca9f4a467be6207.jpg" alt="" /> </a></li>
-          <li><a href="#"><img src="https://i.pinimg.com/564x/9c/91/98/9c919823b4cac48bec5af1f236a39efd.jpg" alt="" /> </a></li>
+          <li><a href="#"><img src="https://i.pinimg.com/564x/13/5c/4e/135c4ec68ba482ea3e2817b30aaf9efe.jpg" alt="" /> </a></li>
+          <li><a href="#"><img src="https://i.pinimg.com/564x/c1/1c/e6/c11ce6b714d8aaed20161a13d4f59fbc.jpg" alt="" /> </a></li>
           <li><a href=""><img src="https://i.pinimg.com/236x/76/8d/9a/768d9aea2e2cbb86a28a258bb3adf3c8.jpg" alt="" /> </a></li>
+          <li><a href=""><img src="https://i.pinimg.com/564x/c6/f0/ec/c6f0ec9138a14b8ff15fbb87258beb5a.jpg" alt="" /> </a></li>
           <li><a href=""><img src="https://i.pinimg.com/564x/28/83/a3/2883a3452d9c69bdf5cd012fa91a8fb8.jpg" alt="" /> </a></li>
           <li><a href=""><img src="https://i.pinimg.com/564x/6f/09/60/6f09603a55feff6cc4a75602d7d9bed1.jpg" alt="" /> </a></li>
           <li><a href=""><img src="https://i.pinimg.com/564x/d7/82/77/d78277d7629c355d049595f85e43756c.jpg" alt="" /> </a></li>
