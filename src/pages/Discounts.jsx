dc008216996ca22cf453156f22252e4b54308136
Nagima Zhumakadyrova <zhumakadyrova7@gmail.com>
2023-05-12 12:58:21 +0600

    work on the conent of the discount's page

--- src/pages/Discounts.jsx
+++ src/pages/Discounts.jsx
@@ -1,11 +1,12 @@
 import "./Home.css";
+import React from "react";
 export default function Discounts() {
   return (
     <div className="Discounts">
       <div className="Main">
-        <h1>Welcome to my Contacts shop</h1>
-        <p>This is my awesome Ice-Cream shop.</p>
-        <p>We only use natural ingredients to make our yummy ice-creams</p>
+        <h1>Special days</h1>
+        <p>Tours from 5 USD</p>
+        <p>Don' miss your chance. To be a part of our adventures</p>
       </div>
     </div>
   );
