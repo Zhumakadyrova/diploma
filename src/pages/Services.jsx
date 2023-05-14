4054a8493c9b7b7d1d07d5dd9b0562ca50640936
Nagima Zhumakadyrova <zhumakadyrova7@gmail.com>
2023-05-14 22:43:12 +0600

    add row of questions to the service's page

--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -1,4 +1,4 @@
-import "./Home.css";
+import "./Services.css";
 import React from "react";
 export default function Services() {
   return (
@@ -7,6 +7,45 @@ export default function Services() {
         <h1>Welcome to my Contacts shop</h1>
         <p>This is my awesome Ice-Cream shop.</p>
         <p>We only use natural ingredients to make our yummy ice-creams</p>
+      </div>
+      <div className="question_list">
+        <div className="question_row">
+          <div className="col-4">
+            <h5>snflnf</h5>
+            <p>Lorem ipsum dolor sit amet.</p>
+          </div>
+          <div className="col-8">
+            <p>About</p>
+          </div>
+        </div>
+        <div className="question_row">
+          <div className="col-4">
+            <h5>snflnf</h5>
+            <p>Lorem ipsum dolor sit amet.</p>
+          </div>
+          <div className="col-8">
+            <p>About</p>
+          </div>
+        </div>
+        <div className="question_row">
+          <div className="col-4">
+            <h5>snflnf</h5>
+            <p>Lorem ipsum dolor sit amet.</p>
+          </div>
+          <div className="col-8">
+            <p>About</p>
+          </div>
+        </div>
+        <div className="question_row">
+          <div className="col-4">
+            <h5>snflnf</h5>
+            <p>Lorem ipsum dolor sit amet.</p>
+          </div>
+          <div className="col-8">
+            <p>About</p>
+          </div>
+        </div>
+        
       </div>
     </div>
   );
