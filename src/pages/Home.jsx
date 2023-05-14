9b91a83ace38ada4ecf19e4ec3cff3b80c82fd70
Nagima Zhumakadyrova <zhumakadyrova7@gmail.com>
2023-05-14 22:42:29 +0600

    add extra link to the contacts.jsx and give gradient effect

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,8 +5,13 @@ export default function Home() {
     <div className="Home">
       <div className="Main">
         <h1>Welcome to Try Travel</h1>
-        <p>This is my awesome Ice-Cream shop.</p>
+        <h4>Your adventure starts here</h4>
         <p>We only use natural ingredients to make our yummy ice-creams</p>
+      </div>
+      <div className="Cntct">
+        <div className="Cnct_link">
+          <a href="./Contacts.jsx" className="Cnct_link">Contact us </a>
+        </div>
       </div>
     </div>
   );
