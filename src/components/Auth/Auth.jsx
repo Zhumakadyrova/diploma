97c6431a7ce710fdaf8ef5ba4e0c2bb2f766c518
Nagima Zhumakadyrova <zhumakadyrova7@gmail.com>
2023-05-16 18:16:29 +0600

    trying to replace user.name to the img

--- src/components/Auth/Auth.jsx
+++ src/components/Auth/Auth.jsx
@@ -3,6 +3,7 @@ import "./Auth.css";
 import { AppContext } from "../../App";
 import { logIn, logOut } from "../../firebase";
 import { Link } from "react-router-dom";
+import userIicon from "../../assets/contacts_icons/auth.jpg";
 
 export default function Auth() {
   const { user } = useContext(AppContext);
@@ -10,14 +11,14 @@ export default function Auth() {
   // показывается гостю
   let output = (
     <span className="Login">
-      Guest <button onClick={logIn}>Sign in</button>
+      <img src="src/components/Auth/Auth.jsxC:/Users/User/Desktop/IT academy/diploma/src/assets/contacts_icons/auth_picture.jpg" alt="" /> <button onClick={logIn}>Sign in</button>
     </span>
   );
   // показывается пользователю
   if (user) {
     output = (
       <span className="Logout">
-        <Link to={"/orders"} className="OrderLink">{user.displayName}</Link>
+        <Link to={"/orders"} className="OrderLink"> <img src={userIicon} alt="" /></Link>
         <button onClick={logOut}>Sign out</button>
       </span>
     );
