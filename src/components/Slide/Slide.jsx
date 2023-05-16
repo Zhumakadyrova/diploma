290b8161a8372867878a65952e248f97415af4ca
Nagima Zhumakadyrova <zhumakadyrova7@gmail.com>
2023-05-16 06:32:26 +0600

    try to add slider code and create a component

---
+++ src/components/Slide/Slide.jsx
@@ -0,0 +1,15 @@
+// import React from 'react';
+
+// function Slide() {
+//   return (
+//     <div className='slide'>
+//       <img src='https://example.com/image.jpg' alt='Example' />
+//       <div className='slide-description'>
+//         <h2>Example Slide</h2>
+//         <p>This is an example of a slide that shows an image and description.</p>
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default Slide;
