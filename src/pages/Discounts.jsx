4b361ba8489f5b65a0c1e93f12cfb102fd67f95d
Nagima Zhumakadyrova <zhumakadyrova7@gmail.com>
2023-05-16 18:10:32 +0600

    add extra cards with new styles

--- src/pages/Discounts.jsx
+++ src/pages/Discounts.jsx
@@ -74,12 +74,86 @@ export default function Discounts() {
             />
           </div>
           <div className="card-text">
-            Lorem ipsum dolor sit amet. <br />
+            
+          </div>
+        </div>
+      </div>
+      <div className="flip-card">
+        <div className="flip-card-inner">
+          <div className="flip-card-front">
+            <img
+              src="https://i.pinimg.com/564x/39/5b/26/395b26a876388aa94d0f9b7cddc70833.jpg"
+              alt="Avatar"
+            />
+          </div>
+          <div className="flip-card-back">
+            <p>Lorem ipsum dolor sit amet.</p>
             <span>50$</span> <br />
             <a href="#"> Learn more</a>
           </div>
         </div>
       </div>
+      <div className="flip-card">
+        <div className="flip-card-inner">
+          <div className="flip-card-front">
+            <img
+              src="https://i.pinimg.com/564x/39/5b/26/395b26a876388aa94d0f9b7cddc70833.jpg"
+              alt="Avatar"
+            />
+          </div>
+          <div className="flip-card-back">
+            <p>Lorem ipsum dolor sit amet.</p>
+            <span>50$</span> <br />
+            <a href="#"> Learn more</a>
+          </div>
+        </div>
+      </div>
+      <div className="flip-card">
+        <div className="flip-card-inner">
+          <div className="flip-card-front">
+            <img
+              src="https://i.pinimg.com/564x/39/5b/26/395b26a876388aa94d0f9b7cddc70833.jpg"
+              alt="Avatar"
+            />
+          </div>
+          <div className="flip-card-back">
+            <p>Lorem ipsum dolor sit amet.</p>
+            <span>50$</span> <br />
+            <a href="#"> Learn more</a>
+          </div>
+        </div>
+      </div>
+      <div className="flip-card">
+        <div className="flip-card-inner">
+          <div className="flip-card-front">
+            <img
+              src="https://i.pinimg.com/564x/39/5b/26/395b26a876388aa94d0f9b7cddc70833.jpg"
+              alt="Avatar"
+            />
+          </div>
+          <div className="flip-card-back">
+            <p>Lorem ipsum dolor sit amet.</p>
+            <span>50$</span> <br />
+            <a href="#"> Learn more</a>
+          </div>
+        </div>
+      </div>
+      <div className="flip-card">
+        <div className="flip-card-inner">
+          <div className="flip-card-front">
+            <img
+              src="https://i.pinimg.com/564x/39/5b/26/395b26a876388aa94d0f9b7cddc70833.jpg"
+              alt="Avatar"
+            />
+          </div>
+          <div className="flip-card-back">
+            <p>Lorem ipsum dolor sit amet.</p>
+            <span>50$</span> <br />
+            <a href="#"> Learn more</a>
+          </div>
+        </div>
+      </div>
+
       <div className="Main_discount">
         <div className="col-6">
           <div className="col-6_content">
@@ -87,10 +161,12 @@ export default function Discounts() {
             <h5>Do you want to go to the round trip?</h5>
             <a href="#">Contact us</a>
           </div>
-
         </div>
         <div className="col-6">
-          <img src="https://i.pinimg.com/564x/91/cf/42/91cf4253d596ee9b523086536f468c07.jpg" alt="" />
+          <img
+            src="https://i.pinimg.com/564x/91/cf/42/91cf4253d596ee9b523086536f468c07.jpg"
+            alt=""
+          />
         </div>
       </div>
     </div>
