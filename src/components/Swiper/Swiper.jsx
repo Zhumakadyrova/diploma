42a92e6e833188ac1a02cc203abfef71fa5261a4
Nagima Zhumakadyrova <zhumakadyrova7@gmail.com>
2023-05-17 21:00:08 +0600

    change the command to stylize auto play of slide

--- src/components/Swiper/Swiper.jsx
+++ src/components/Swiper/Swiper.jsx
@@ -1,36 +1,34 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import "./Swiper.css";
 
 const Slide = () => {
   const [counter, setCounter] = useState(0);
-  const images = ['public/images/1st.jpg', 'public/images/2nd.jpg', 'public/images/3rd.jpg', 'public/images/5th.jpg', ];
+  const images = ['public/images/1st.jpg', 'public/images/2nd.jpg', 'public/images/3rd.jpg', 'public/images/5th.jpg'];
 
-  const incrementCounter = () => {
-    setCounter(prevCounter => (prevCounter + 1) % images.length);
-  };
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCounter(prevCounter => (prevCounter + 1) % images.length);
+    }, 2000); // Изменить слайд каждую секунду
 
-  const decrementCounter = () => {
-    setCounter(prevCounter => (prevCounter - 1 + images.length) % images.length);
-  };
+    return () => {
+      clearInterval(interval); // Очистить интервал при размонтировании компонента
+    };
+  }, []);
 
   return (
     <div className="slide">
       <h2>Our tourist's photo</h2>
       <p>This is a stylish and interactive slide created using React.</p>
       <div className="bg-frame">
-      <div className="image-container">
-        <img src={images[counter]} alt="Slide Image" />
-      </div>
+        <div className="image-container">
+          <img src={images[counter]} alt="Slide Image" />
+        </div>
       </div>
       <div className="counter">
-        <button onClick={decrementCounter}>-</button>
         <span>{counter + 1}/{images.length}</span>
-        <button onClick={incrementCounter}>+</button>
       </div>
     </div>
   );
 };
 
-
 export default Slide;
-
