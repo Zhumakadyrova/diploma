d9c7996ac3feb2f0f04361cc3c2400835339980d
Nagima Zhumakadyrova <zhumakadyrova7@gmail.com>
2023-05-18 19:11:37 +0600

    change the format of the img and transparent

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -1,12 +1,12 @@
 import "./Logo.css";
-import logo from "../../assets/logo.jpg";
+import logo from "../../assets/logo.png";
 import { NavLink } from "react-router-dom";
 
 export default function Logo() {
   return (
     <div className="Logo">
       <NavLink to="/">
-        <img src={logo} alt="Website logo" />
+        <img src={logo} alt="Website logo"  />
       </NavLink>
     </div>
   );
