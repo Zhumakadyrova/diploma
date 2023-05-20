0c1691baa4cb842a1bf0bdaa17e441a63cb57676
Nagima Zhumakadyrova <zhumakadyrova7@gmail.com>
2023-05-20 09:51:58 +0600

    remove extra items to make page more UI rules

--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -47,48 +47,6 @@ export default function Services() {
           </div>
         </div>
       </div>
-      <ul>
-        <li>
-          <a href="#">
-            <img
-              src="https://i.pinimg.com/564x/68/8e/45/688e4524c03c532cf821a8ca0935a6fe.jpg"
-              alt=""
-            />
-          </a>
-        </li>
-        <li>
-          <a href="#">
-            <img
-              src="https://i.pinimg.com/564x/44/db/1a/44db1a7224d40d24bbb32fea56f9f91a.jpg"
-              alt=""
-            />
-          </a>
-        </li>
-        <li>
-          <a href="#">
-            <img
-              src="https://i.pinimg.com/564x/42/61/58/4261587d247054809e5c8d62fa143c3a.jpg"
-              alt=""
-            />
-          </a>
-        </li>
-        <li>
-          <a href="#">
-            <img
-              src="https://i.pinimg.com/564x/6f/af/c5/6fafc54dffe3fb0554d4ddeca1c762ef.jpg"
-              alt=""
-            />
-          </a>
-        </li>
-        <li>
-          <a href="#">
-            <img
-              src="https://i.pinimg.com/564x/71/66/22/7166224b95d9b7f229367e9fe1a8ce08.jpg"
-              alt=""
-            />
-          </a>
-        </li>
-      </ul>
       <Slide />
     </div>
     
