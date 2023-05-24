83234ccd83b8b1e75e2b24c8e614cbec91fbf000
Nagima Zhumakadyrova <zhumakadyrova7@gmail.com>
2023-05-25 01:11:02 +0600

    almost finished aout navidatiob bing the

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,5 +1,5 @@
 import { useContext } from "react";
-import { useMatch } from "react-router-dom";
+import { useMatch, Link } from "react-router-dom";
 import { AppContext } from "../App";
 import NotFound from "./NotFound";
 import AddToCart from "../components/AddToCart/AddToCart";
@@ -10,26 +10,44 @@ export default function Product() {
   const { products } = useContext(AppContext);
 
   const product = products.find((product) => product.path === params.path);
-  
-  if (!product) {
+  const currentIndex = products.findIndex(
+    (product) => product.path === params.path
+  );
+  const previousIndex = currentIndex - 1;
+  const nextIndex = currentIndex + 1;
+  const previous = previousIndex >= 0 ? products[previousIndex] : null;
+  const next = nextIndex < products.length ? products[nextIndex] : null;
+
+  if (!products[currentIndex]) {
     return <NotFound />;
   }
 
   return (
     <div className="Product">
-      
-      <div className="Product-text">
-        <img src={product.picture} alt={product.name} />
-        <div className="card_text">
-        <h3>{product.name}</h3>
-        <span>{product.price} som</span>
-        <AddToCart product={product} />
+      <div className="Product1">
+        <div className="Product-text">
+          <img src={product.picture} alt={product.name} />
+          <div className="card_text">
+            <h3>{product.name}</h3>
+            <span>{product.price} som</span>
+            <AddToCart product={product} />
+          </div>
         </div>
-        
-      
+        <div className="Product_description">
+          <p>{product.description}</p>
+        </div>
       </div>
-      <div className="Product_description">
-      <p>{product.description}</p>
+      <div className="Product_navigation">
+        {previous && (
+          
+            <Link to={`/product/${previous.path}`}><button>Previous</button></Link>
+          
+        )}
+        {next && (
+          
+            <Link to={`/product/${next.path}`}><button>Next</button></Link>
+
+        )}
       </div>
     </div>
   );
