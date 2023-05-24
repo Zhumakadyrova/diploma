b0027f79c4f1d601e64069f92b2d5192ed13a3a7
Nagima Zhumakadyrova <zhumakadyrova7@gmail.com>
2023-05-24 07:41:25 +0600

    add navigation buttons to the swiper and stylize them

--- src/components/Swiper/Swiper.jsx
+++ src/components/Swiper/Swiper.jsx
@@ -8,17 +8,39 @@ import img5 from "../../assets/slide_photo/5th.jpg";
 
 const Slide = () => {
   const [counter, setCounter] = useState(0);
-  const images = ["https://i.pinimg.com/564x/60/88/ec/6088ec1a0b9af7edfd275e9984c214cb.jpg", img1, img2, img3, img4, img5 ];
+  const [isPaused, setIsPaused] = useState(false);
+  const images = [
+    "https://i.pinimg.com/564x/60/88/ec/6088ec1a0b9af7edfd275e9984c214cb.jpg",
+    img1,
+    img2,
+    img3,
+    img4,
+    img5
+  ];
+
+  const nextSlide = () => {
+    setCounter(prevCounter => (prevCounter + 1) % images.length);
+  };
+
+  const prevSlide = () => {
+    setCounter(prevCounter => (prevCounter - 1 + images.length) % images.length);
+  };
+
+  const togglePause = () => {
+    setIsPaused(prevIsPaused => !prevIsPaused);
+  };
 
   useEffect(() => {
-    const interval = setInterval(() => {
-      setCounter(prevCounter => (prevCounter + 1) % images.length);
-    }, 2000); // Изменить слайд каждую секунду
+    let interval = null;
+
+    if (!isPaused) {
+      interval = setInterval(nextSlide, 2000);
+    }
 
     return () => {
-      clearInterval(interval); // Очистить интервал при размонтировании компонента
+      clearInterval(interval);
     };
-  }, []);
+  }, [isPaused]);
 
   return (
     <div className="slide">
@@ -31,6 +53,11 @@ const Slide = () => {
       </div>
       <div className="counter">
         <span>{counter + 1}/{images.length}</span>
+      </div>
+      <div className="controls">
+        <button onClick={prevSlide}>Previous</button>
+        <button onClick={togglePause}>{isPaused ? 'Play' : 'Pause'}</button>
+        <button onClick={nextSlide}>Next</button>
       </div>
     </div>
   );
